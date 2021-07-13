56d884a40304358f8f2251ae1b94d004d8c28eba
Arman <armnet-10@mail.ru>
2021-07-13 21:46:49 +0400

    fixed component

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,7 @@
             @if (config('apricode.widgets.default.account', true))
                 <x-filament::card class="flex">
                     <div class="flex items-center space-x-4 rtl:space-x-reverse">
-                        <x-filament-avatar :user="\Filament\Filament::auth()->user()" :size="160" class="flex-shrink-0 w-20 h-20 rounded-full" />
+                        <x-filament::avatar :user="\Filament\Filament::auth()->user()" :size="160" class="flex-shrink-0 w-20 h-20 rounded-full" />
 
                         <div class="space-y-1">
                             <h2 class="text-2xl">{{ __('filament::dashboard.widgets.account.heading', ['name' => \Filament\Filament::auth()->user()->name]) }}</h2>
