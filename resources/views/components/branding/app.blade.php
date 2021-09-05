512af6da00f73dc2f3662a512caeae2d1f756286
Arman <armnet-10@mail.ru>
2021-09-05 05:53:52 +0400

    Update `filament` version

--- resources/views/components/branding/app.blade.php
+++ resources/views/components/branding/app.blade.php
@@ -1,6 +1,6 @@
 <a
     href="{{ url('/').'/'.config('apricode.path') }}"
-    rel="home"
+    rel="home noopener noreferrer"
     class="flex items-center p-4 space-x-3 rtl:space-x-reverse group"
 >
     <img src="{{asset(config('apricode.logo_path'))}}" alt="{{config('apricode.product_name')}} Logo" width="40" height="40" class="w-10 h-10">
